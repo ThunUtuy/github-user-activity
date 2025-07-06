f7e0f9d2169a9d2cb93bd2decf0e6486529577f2
Thun Vaniyapong <thun.vaniya@gmail.com>
2025-07-06 14:25:02 +0700

    handle different events

--- github_api.py
+++ github_api.py
@@ -6,7 +6,28 @@ def get_event(user):
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        for event in data:
+            # Covering only the most common events
+            if event['type'] == 'IssueCommentEvent':
+                print(f"- :smiley: commented on issue {event['payload']['issue']['number']}")
+            elif event['type'] == 'PushEvent':
+                print(f"- :smiley: pushed {event["payload"]["size"]} commits to {event['repo']['name']}")
+            elif event['type'] == 'IssuesEvent':
+                print(f"- :smiley: created issue {event['payload']['issue']['number']}")
+            elif event['type'] == 'WatchEvent':
+                print(f"- :smiley: starred {event['repo']['name']}")
+            elif event['type'] == 'PullRequestEvent':
+                print(f"- :smiley: created pull request {event['payload']['pull_request']['number']}")
+            elif event['type'] == 'PullRequestReviewEvent':
+                print(f"- :smiley: reviewed pull request {event['payload']['pull_request']['number']}")
+            elif event['type'] == 'PullRequestReviewCommentEvent':
+                print(f"- :smiley: commented on pull request {event['payload']['pull_request']['number']}")
+            elif event['type'] == 'CreateEvent':
+                print(f"- :smiley: created {event['payload']['ref_type']} {event['payload']['ref']}")
+            else:
+                print(f"- :smiley: {event['type']}")
+    else:
+        print(f"Error fetching response for {user} - response code: {response.status_code}")
 
 
 parser = argparse.ArgumentParser(prog = 'task-cli')
